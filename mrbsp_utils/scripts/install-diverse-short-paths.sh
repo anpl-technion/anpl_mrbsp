8bc034d602e112d6623e27f7f7d4e320dd60ad94
andrej <akitanov@gmail.com>
2019-11-24 14:13:52 +0200

    adding util scripts

---
+++ mrbsp_utils/scripts/install-diverse-short-paths.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../../action_generator_node/3rdparty/diverse_short_paths/
+sudo make install -j3
---
+++ mrbsp_utils/scripts/install-find-cmakes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# copy necessarily cmake files to prefix path
+PREFIX=/usr/ANPLprefix
+sudo cp -R cmake $PREFIX/share/
