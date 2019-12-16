0236bc66447810e1adf935accb02b82d8a2c2d1d
jeniabb <evgenykoretsky@gmail.com>
2019-12-16 13:03:15 +0200

    change roslaunch from topological to passive demo

--- scenarios/anpl_passive_demo_sr/scripts/run_robot_sensors_A.sh
+++ scenarios/anpl_passive_demo_sr/scripts/run_robot_sensors_A.sh
@@ -6,4 +6,4 @@ export $ROBOT_NAME
 sudo chmod a+rw /dev/ttyUSB0
 sudo chmod 777 /dev/ttyACM0
 
-roslaunch topological_mr_active launch_robot_sensors.launch robot_name:=$ROBOT_NAME is_lidar_usb:=True
+roslaunch roslaunch anpl_passive_demo_sr launch_robot_sensors.launch robot_name:=$ROBOT_NAME is_lidar_usb:=True
